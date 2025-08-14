125f4bdd4407344849a8084e6cfa7b0e0d41d94b
skksanjaya <kasun.eng@gmail.com>
2025-08-14 18:27:51 +1200

    Update personalDetails.py

--- week2/personalDetails.py
+++ week2/personalDetails.py
@@ -1,9 +1,13 @@
 class personDetail:
-    def __init__(self,name,age,address):
+    def __init__(self,name: str,age: int,address: str):
         self.details=[name,age,address]
 
-    def get_data(self,i):
+    def get_data(self,i: int):
         return self.details[i]
+    
+    def update_age(self,add_age: int):
+        self.details[1]=int(self.details[1])+int(add_age)
+        return self.details[1]
     
 def main():
     name=input("Enter your Name: ")
@@ -13,6 +17,9 @@ def main():
     print(f"Name: {p1.get_data(0)}")
     print(f"Age: {p1.get_data(1)}")
     print(f"Address: {p1.get_data(2)}")
+    years=input("Enter to add years: ")
+    print(f"New Age: {p1.update_age(years)}")
+
 
     
 if __name__ == "__main__":
