03838b9470054302100993007271207abc5669fd
skksanjaya <kasun.eng@gmail.com>
2025-08-30 10:14:55 +1200

    Update ParentInitializer.py

--- week5/ParentInitializer.py
+++ week5/ParentInitializer.py
@@ -3,13 +3,18 @@ class Person:
         self.name=name
         self.address=address
         self.age=age
+
+    def greet(self):
+        print("Greetings : "+self.name)  
+
 class Student(Person):
     def __init__(self,name,address,age,studentID):
         super().__init__(name,address,age) 
         self.studentID=studentID
 
     def greet(self):
-        print("Greetings : "+self.name)    
+        print("Greetings : "+self.name +self.address)  
+        super().greet()  
 
 class Staff(Person):
     def __init__(self,taxCode):
@@ -24,7 +29,7 @@ class General(Staff):
         self.payRate=payRate 
 
 def main():
-    student1=Student("Nimal","Quenn street",25,"S0001")
+    student1=Student("Nimal","Quenn street",25,"S0001")  
     student1.greet()
 
 if __name__ == "__main__":
